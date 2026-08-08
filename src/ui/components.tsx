4d131562e8b17aac75d7d7ddd25374750b476f60
太夜 <168279057+TAI-YE-1@users.noreply.github.com>
2026-08-08 20:18:01 +0800

    refactor(ui): remove retired selection menu alias

--- src/ui/components.tsx
+++ src/ui/components.tsx
@@ -1,9 +1,6 @@
 export { AiSheet, DocumentSheet } from './LazySheets'
 export { LintCard } from './LintCard'
 export { Preview } from './Preview'
-export {
-  SelectionActionBar,
-  SelectionActionBar as SelectionContextMenu,
-} from './SelectionActionBar'
+export { SelectionActionBar } from './SelectionActionBar'
 export { SlashMenu } from './SlashMenu'
 export { SuggestionCard } from './SuggestionCard'
